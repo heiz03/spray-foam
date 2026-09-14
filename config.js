bc14e9debaf59ba47286076e550603981f7176dd
Joshua <jpernito003@gmail.com>
2026-09-15 04:03:02 +0800

    aw

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // WEB3FORMS API KEY: replace the placeholder below with your Web3Forms access key.
-window.WEB3FORMS_ACCESS_KEY = "YOUR_WEB3FORMS_ACCESS_KEY_HERE";
+window.WEB3FORMS_ACCESS_KEY = "236c4c7a-4fca-465f-9573-488d44d73a57";
